eb00c4a23536396a74a4cf01919ecb29112e946a
Maksim Fesenko <drresist@gmail.com>
2023-07-22 15:36:50 +0300

    init

---
+++ README.md
@@ -0,0 +1,40 @@
+# Awaker bot
+
+Welcome to the Telegram Weather and Birthday Bot project! This Python script is designed to retrieve weather information for Moscow from the OpenWeatherMap API and notify users about birthdays from a CSV file.
+
+The bot will send a formatted message containing weather data and any upcoming birthdays to a specified chat using Telegram's Bot API.
+Prerequisites
+
+Before running the bot, make sure you have the following:
+
+    Python (>= 3.6)
+    OpenWeatherMap API Key: Obtain an API key from OpenWeatherMap to access weather data.
+    Telegram Bot API Key: Create a Telegram bot and get the API token from BotFather.
+    Chat ID: Identify the chat ID where the bot will send messages. This can be your private chat ID or a group chat ID.
+
+## Installation
+
+Clone this repository to your local machine.
+
+    git clone https://github.com/your-username/telegram-weather-bot.git
+Change into the project directory.
+
+    cd telegram-weather-bot
+
+Install the required Python dependencies using pip.
+
+    pip install -r requirements.txt
+
+Create a .env file in the project root and add your API keys and chat ID.
+
+    OW_API=YOUR_OPENWEATHERMAP_API_KEY
+    TG_BOT_API=YOUR_TELEGRAM_BOT_API_KEY
+    CHAT_ID=YOUR_CHAT_ID
+
+## Usage
+
+To run the Telegram Weather and Birthday Bot, execute the main() function in the bot.py script.
+
+    python bot.py
+
+The bot will start running and will send periodic messages to the specified chat with weather information and upcoming birthdays for the day.
---
+++ main.py
@@ -0,0 +1,97 @@
+from __future__ import annotations
+
+import csv
+import json
+import os
+import time
+from loguru import logger
+import requests
+import telebot
+from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OW_API = os.getenv("OW_API")
+TG_BOT_API = os.getenv("TG_BOT_API")
+CHAT_ID = os.getenv("CHAT_ID")
+
+logger.add("app.log", retention="10 days")  # Cleanup after some time
+
+
+def get_weather() -> str | None:
+    """
+    Request weather from openWeatherApi and format
+    :return: str if ok, else None
+    """
+    url = f"http://api.openweathermap.org/data/2.5/weather?q=Moscow&appid={OW_API}&lang=ru&units=metric"
+    weather_data = requests.get(url)
+    logger.info(f"Requesting weather data from OW")
+    with open('icons.json', 'r', encoding='utf-8') as f:
+        icons = json.load(f)
+    logger.info("Weather data received with status code: " + str(weather_data.status_code))
+    if weather_data.status_code == 200:
+        weather_data = weather_data.json()
+        text = f"Погода {icons[weather_data['weather'][0]['icon']]}: {int(weather_data['main']['temp'])}°C" \
+               f" (ощ. {int(weather_data['main']['feels_like'])}°C), " \
+               f"{weather_data['weather'][0]['description']}"
+        logger.info(text)
+        return text
+    else:
+        return ""
+
+
+def get_birthday() -> str | None:
+    """
+    Read birthday csv file in format "Name", "DD-MM"
+    :return: return Name or None
+    """
+    names = []
+    with open("./birthdays.csv", "r") as file:
+        csv_file = csv.DictReader(file)
+        for line in csv_file:
+            if line["date"] == f"{datetime.today().day}-{datetime.today().month}":
+                names.append(line["Name"])
+        logger.info(f"Found {len(names)} names")
+    if len(names) == 0:
+        return ""
+    else:
+        return "День рождения у 🎂: \n" + '\n'.join(names)
+
+
+def create_message() -> str:
+    """
+    Format message for sending by tgbot
+    :return: formatted str
+    """
+    return f"*Всем привет!👋*\n" \
+           f"{get_weather()}\n" \
+           f"{get_birthday()}\n"
+
+
+def send_message(text: str) -> None:
+    """
+    Send formatted message
+    :return:
+    """
+    bot = telebot.TeleBot(token=TG_BOT_API)
+    logger.info(f"Sending message {text}")
+    bot.send_message(
+        text=text,
+        chat_id=CHAT_ID,
+        disable_notification=True,
+        parse_mode="markdown"
+    )
+
+
+def main() -> None:
+    while True:
+        try:
+            send_message(create_message())
+        except Exception as e:
+            logger.error(f"An error occurred: {e}")
+        time.sleep(3600)
+
+
+if __name__ == '__main__':
+    main()
